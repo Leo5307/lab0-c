61f443e33ebe571b95f24df6643313f179f2b00d
Leo5307 <inquisitorleo0123@gmail.com>
2024-03-04 00:06:45 +0800

    Add q_remove_head and q_remove_tail

--- queue.c
+++ queue.c
@@ -98,13 +98,36 @@ bool q_insert_tail(struct list_head *head, char *s)
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 
 {
-    return NULL;
+    if (!head) {
+        return NULL;
+    }
+    element_t *remove_node = list_first_entry(head, element_t, list);
+    if (sp && remove_node) {
+        strncpy(sp, remove_node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+        list_del(&remove_node->list);
+        printf("%s", sp);
+    }
+
+    return remove_node;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head) {
+        return NULL;
+    }
+    element_t *remove_node = list_last_entry(head, element_t, list);
+    if (sp && remove_node) {
+        strncpy(sp, remove_node->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+        list_del(&remove_node->list);
+        printf("%s", sp);
+    }
+
+    return remove_node;
+    // return NULL;
 }
 
 /* Return number of elements in queue */
